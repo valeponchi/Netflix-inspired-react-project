a14fa97ec2bed2ff07cbeb2817b333b90d3f1ba9
valeponchi <81304855+valeponchi@users.noreply.github.com>
2021-07-07 22:57:31 +0100

    when hovering on a movie, it shows some details

--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
 import create from 'zustand'
+// const apiKey = 'c0341e4fa2480ab7786c426cfdc94215'
 
 const useStore = create((set, get) => ({
-	popularMovies: {},
+	popularMovies: [],
 	fetchPopularMovies: () => {
 		fetch(
 			'https://api.themoviedb.org/3/movie/popular?api_key=c0341e4fa2480ab7786c426cfdc94215&language=en-US&page=1'
 		)
 			.then(resp => resp.json())
-			.then(movies => set({ popularMovies: movies }))
+			.then(movies => set({ popularMovies: movies.results }))
 	},
 }))
 
@@ -21,3 +22,6 @@ export default useStore
 
 // URL GENRES
 //https://api.themoviedb.org/3/genre/movie/list?api_key=c0341e4fa2480ab7786c426cfdc94215&language=en-US
+
+//https://api.themoviedb.org/3/discover/movie/api_key=c0341e4fa2480ab7786c426cfdc94215&with_genres=35
+//
